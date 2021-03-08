3c3ab4596465de461572f2c954299fdce6b9fae8
AH TH <gutsinbox@gmail.com>
2021-03-08 19:30:29 +0400

    commit

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //я ебал меня сосали
+            //and another one
     }
 }
