c42b70bf7b1ca468bf935376e4d1ad63da4a91fd
Dushanjk <andreyokunev05@mail.ru>
2021-03-08 19:32:40 +0300

    test

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //я ебал меня сосали
             //and another one
+        //вот ю вонт э
     }
 }
